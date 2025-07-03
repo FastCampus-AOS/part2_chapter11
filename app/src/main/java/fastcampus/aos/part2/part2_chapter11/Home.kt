ec4d1e2b5ddce1ae4ff5061044b4eaf5c7702772
ParkHyunSu99 <phs0767@pineone.com>
2025-07-04 03:14:34 +0900

    홈 화면 UI 구성하기 (2)

--- app/src/main/java/fastcampus/aos/part2/part2_chapter11/Home.kt
+++ app/src/main/java/fastcampus/aos/part2/part2_chapter11/Home.kt
@@ -3,10 +3,16 @@ package fastcampus.aos.part2.part2_chapter11
 data class Home(
     val user: User,
     val appbarImage: String,
+    val banner: Banner,
 )
 
 data class User(
     val nickname: String,
     val starCount: Int,
     val totalCount: Int,
+)
+
+data class Banner(
+    val image: String,
+    val contentDescription: String
 )
--- app/src/main/java/fastcampus/aos/part2/part2_chapter11/HomeFragment.kt
+++ app/src/main/java/fastcampus/aos/part2/part2_chapter11/HomeFragment.kt
@@ -1,5 +1,6 @@
 package fastcampus.aos.part2.part2_chapter11
 
+import android.animation.ValueAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -16,22 +17,76 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHomeBinding.bind(view)
 
-        val homeData = context?.readData() ?: return
+        val homeData = context?.readData("home.json", Home::class.java) ?: return
+        val menuData = context?.readData("menu.json", Menu::class.java) ?: return
+
+        initAppBar(homeData)
+        initRecommandList(homeData, menuData)
+        initBannerLayout(homeData)
+        initFoodList(menuData)
+
+        binding.scrollView.setOnScrollChangeListener { v, _, scrollY, _, _ ->
+            if (scrollY == 0) {
+                binding.floatingActionButton.extend()
+            } else {
+                binding.floatingActionButton.shrink()
+            }
+        }
+
+
+
+    }
+
+    private fun initFoodList(menuData: Menu) {
+        binding.foodMenuList.titleTextView.text = getString(R.string.food_menu_title)
+        menuData.food.forEach { menuItem ->
+            binding.foodMenuList.menuLayout.addView(
+                MenuView(context = requireContext()).apply {
+                    setTitle(menuItem.name)
+                    setImageUrl(menuItem.image)
+                }
+            )
+        }
+    }
+
+    private fun initBannerLayout(homeData: Home) {
+        binding.bannerLayout.bannerImageView.apply {
+            Glide.with(this).load(homeData.banner.image).into(this)
+            this.contentDescription = homeData.banner.contentDescription
+        }
+    }
+
+    private fun initRecommandList(
+        homeData: Home,
+        menuData: Menu
+    ) {
+        binding.recommendMenuList.titleTextView.text =
+            getString(R.string.recommend_title, homeData.user.nickname)
+        menuData.coffee.forEach { menuItem ->
+            binding.recommendMenuList.menuLayout.addView(
+                MenuView(context = requireContext()).apply {
+                    setTitle(menuItem.name)
+                    setImageUrl(menuItem.image)
+                }
+            )
+        }
+    }
+
+    private fun initAppBar(homeData: Home) {
         binding.appBarTitleTextView.text = getString(R.string.appbar_title_text, homeData.user.nickname)
         binding.starCountView.text = getString(R.string.app_bar_start_count, homeData.user.starCount, homeData.user.totalCount)
 
-        Glide.with(binding.appBarImageView).load(homeData.appbarImage).into(binding.appBarImageView)
-
-        binding.appBarProgressBar.progress = homeData.user.starCount
         binding.appBarProgressBar.max = homeData.user.totalCount
 
-        binding.recommendMenuList.menuLayout.addView(
-            MenuView(context = requireContext()).apply {
-                setTitle("디카페인 카페라떼")
-                setImageUrl("https://picsum.photos/100/100")
-            }
-        )
+        Glide.with(binding.appBarImageView).load(homeData.appbarImage).into(binding.appBarImageView)
 
+        ValueAnimator.ofInt(0, homeData.user.starCount).apply {
+            duration = 1000
+            addUpdateListener {
+                binding.appBarProgressBar.progress = it.animatedValue as Int
+            }
+            start()
+        }
     }
 
 }
